83ebaa237995bee83a8633788c64b8b140b54a87
MahadAbdi1 <219122822@mycput.ac.za>
2022-12-01 10:57:40 -0800

    task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10):
+    for k in range(i+1, 10):
+        if i == 8 and k == 9:
+            print("{}{}".format(i, k))
+        elif i != k:
+            print("{}{}".format(i, k), end=", ")
