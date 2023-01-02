62de774574c658be4b32ddb07e186a7e07496011
MahadAbdi1 <219122822@mycput.ac.za>
2023-01-02 13:49:06 -0800

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,8 @@
+def add_integer(a, b=98):
+    if not (isinstance(a, int) 
+   or isinstance(a, float)) or not (isinstance(b, int) or isinstance(b, float)):
+        raise TypeError('a and b must be integers or floats')
+    a = int(a)
+    b = int(b)
+    return a + b
+
